c0083ec7d865845b0ef5136d7cc9da544f4da7e5
kadirikumar-uptycs <kadirikumar@uptycs.com>
2024-04-30 20:11:10 +0530

    Added Login and Home Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,31 @@
 import React from 'react';
+import {
+    createBrowserRouter,
+    createRoutesFromElements,
+    Route,
+    RouterProvider,
+} from "react-router-dom";
+import LoginPage from './Login/LoginPage';
+import HomePage from './Home/HomePage';
 
-const App = () => {
+const router = createBrowserRouter(
+    createRoutesFromElements(
+        <Route path="/">
+            <Route
+                path="login"
+                element={<LoginPage />}
+            />
+            <Route
+                index
+                element={<HomePage />}
+            />         
+        </Route>
+    )
+);
+
+export default function App() {
+
     return (
-        <div>
-            Hello
-        </div>
-    );
+            <RouterProvider router={router} />
+    )
 }
-
-export default App;
