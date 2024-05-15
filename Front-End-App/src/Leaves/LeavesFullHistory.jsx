edd39c852771241e9ec046363918882b6d069277
kadirikumar-uptycs <kadirikumar@uptycs.com>
2024-05-16 01:36:13 +0530

    Added Filters for leave type and status type and also added clear filter button

--- Front-End-App/src/Leaves/LeavesFullHistory.jsx
+++ Front-End-App/src/Leaves/LeavesFullHistory.jsx
@@ -1,7 +1,12 @@
 import React, { useLayoutEffect } from 'react';
+import { useNavigate, useSearchParams } from 'react-router-dom';
 import TableSortAndSelection from '../common/TableSortAndSelection';
 import { formatDate, getEndDate } from '../common/getCurrentDate';
+import Tooltip from '@mui/joy/Tooltip';
 import StatusButton from '../common/StatusButton';
+import RadioGroup from '../common/RadioGroup';
+import IconButton from '@mui/joy/IconButton';
+import FilterAltOffIcon from '@mui/icons-material/FilterAltOff';
 const headCells = [
     {
         id: 'type',
@@ -37,7 +42,7 @@ const headCells = [
 ];
 
 
-const rows = [
+let rows = [
     {
         id: '1',
         type: 'Sick',
@@ -45,7 +50,7 @@ const rows = [
         to: formatDate(getEndDate(Date.now(), -48)),
         totalDays: 3,
         status: {
-            data: <StatusButton status="Approved"/>,
+            data: <StatusButton status="Approved" />,
             content: "Approved",
         },
     },
@@ -56,7 +61,7 @@ const rows = [
         to: formatDate(getEndDate(Date.now(), -30)),
         totalDays: 2,
         status: {
-            data: <StatusButton status="Rejected"/>,
+            data: <StatusButton status="Rejected" />,
             content: "Rejected",
         },
     },
@@ -67,7 +72,7 @@ const rows = [
         to: formatDate(getEndDate(Date.now(), -3)),
         totalDays: 1,
         status: {
-            data: <StatusButton status="Approved"/>,
+            data: <StatusButton status="Approved" />,
             content: "Approved",
         },
     },
@@ -78,7 +83,7 @@ const rows = [
         to: formatDate(getEndDate(Date.now(), -6)),
         totalDays: 5,
         status: {
-            data: <StatusButton status="Rejected"/>,
+            data: <StatusButton status="Rejected" />,
             content: "Rejected",
         },
     },
@@ -89,21 +94,80 @@ const rows = [
         to: formatDate(getEndDate(Date.now(), 5)),
         totalDays: 5,
         status: {
-            data: <StatusButton status="Pending"/>,
+            data: <StatusButton status="Pending" />,
             content: "Pending",
         },
     }
 ];
 
 const LeavesFullHistory = () => {
+    let navigate = useNavigate();
+    let [searchParams] = useSearchParams();
+    let leaveType = searchParams.get("type");
+    let statusType = searchParams.get("status");
+
     useLayoutEffect(() => {
         document.title = 'Leave History';
     }, []);
+
+    function handletypeChange(event, index, option) {
+        navigate(`/leaveHistory?type=${option}${(statusType) ? `&status=${statusType}` : ''}`);
+    }
+    function handleStatusChange(event, index, option) {
+        navigate(`/leaveHistory?${(leaveType) ? `type=${leaveType}&` : ''}status=${option}`);
+    }
+    function getfilteredData() {
+        let temp_rows = rows.map((item) => item);
+        if ((leaveType && leaveType !== 'All')) {
+            temp_rows = rows.filter(item => item.type === leaveType);
+        }
+        if (statusType && ['Approved', 'Rejected', 'Pending'].includes(statusType)) {
+            return temp_rows && temp_rows.filter(item => item.status.content === statusType);
+        }
+        return temp_rows;
+    }
     return (
         <div>
+            <div style={{
+                marginBottom: '20px',
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'space-evenly'
+            }}>
+                <RadioGroup
+                    type="leave-type"
+                    first="All"
+                    second="Sick"
+                    last="Casual"
+                    handleChangeEvent={handletypeChange}
+                    defaultValue={leaveType ? leaveType: null}
+                />
+                <div onClick={
+                    () => navigate('/leaveHistory')
+                }>
+                <Tooltip arrow title="Clear Filters" color='primary' placement='top'>
+                    <IconButton>
+                        <FilterAltOffIcon sx={{
+                            color: '#222bcc',
+                            padding: '3px',
+                            fontSize: '31px'
+                        }} />
+                    </IconButton >
+                </Tooltip>
+                </div>
+                <RadioGroup
+                    type="status-type"
+                    first="Approved"
+                    second="Pending"
+                    last="Rejected"
+                    handleChangeEvent={handleStatusChange}
+                    defaultValue={(statusType && ['Approved', 'Rejected', 'Pending'].includes(statusType)) ? statusType : null}
+                />
+                
+            </div>
             <TableSortAndSelection
                 headCells={headCells}
-                rows={rows}
+                rows={getfilteredData()}
                 toolBarParams={{
                     title: 'Applied Leaves',
                     styles: {
