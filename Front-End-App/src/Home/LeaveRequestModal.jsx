e849e753ed8633fcc9c740b1f7c0b040f7422c57
kadirikumar-uptycs <kadirikumar@uptycs.com>
2024-05-16 22:12:02 +0530

    Replaced Material Modal with Joy Modal

--- Front-End-App/src/Home/LeaveRequestModal.jsx
+++ Front-End-App/src/Home/LeaveRequestModal.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import Box from '@mui/material/Box';
-import Modal from '@mui/material/Modal';
+import Modal from '@mui/joy/Modal';
+import Grow from '@mui/material/Grow';
 import FormControl from '@mui/material/FormControl';
 import Select from '@mui/material/Select';
 import MenuItem from '@mui/material/MenuItem';
@@ -17,7 +18,7 @@ const LeaveRequestModal = ({ open, onClose }) => {
         position: 'absolute',
         top: '50%',
         left: '50%',
-        transform: 'translate(-50%, -50%)',
+        transform: 'translate(-50%, -50%) !important',
         width: 890,
         height: 690,
         bgcolor: 'background.paper',
@@ -40,126 +41,128 @@ const LeaveRequestModal = ({ open, onClose }) => {
             }}
             disableEscapeKeyDown
         >
-            <Box sx={style}>
-                <h2 className='Heading'>Apply Leave</h2>
-                <div className="body">
-                    <div className="col col-1">
-                        <div className="group">
-                            <span className="title">Reporting Manager</span>
-                            <FormControl sx={{ minWidth: 120 }} size="small">
-                                <Select
-                                    value={manager}
-                                    onChange={handleChange}
-                                    displayEmpty
-                                    inputProps={{ 'aria-label': 'Without label' }}
+            <Grow in={open}>
+                <Box sx={style}>
+                    <h2 className='Heading'>Apply Leave</h2>
+                    <div className="body">
+                        <div className="col col-1">
+                            <div className="group">
+                                <span className="title">Reporting Manager</span>
+                                <FormControl sx={{ minWidth: 120 }} size="small">
+                                    <Select
+                                        value={manager}
+                                        onChange={handleChange}
+                                        displayEmpty
+                                        inputProps={{ 'aria-label': 'Without label' }}
+                                        sx={{
+                                            color: '#000',
+                                            fontFamily: 'Poppins-ExtraBold',
+                                            fontWeight: '300',
+                                            height: '50'
+                                        }}
+                                        className='input'
+                                    >
+                                        <MenuItem value={1}>Sri Rajasekaran</MenuItem>
+                                        <MenuItem value={2}>Kiran Wali</MenuItem>
+                                    </Select>
+                                </FormControl>
+                            </div>
+                            <div className="group">
+                                <span className="title">Leaves Left</span>
+                                <span className='input'>You have used <strong>8.5 Leaves</strong> upto this point</span>
+                            </div>
+                        </div>
+                        <div className="col col-2 group">
+                            <span className="title">Leave Type</span>
+                            <div className="input">
+                                <RadioGroup type="leave-type" first="Sick Leave" second="Paid Leave" />
+                            </div>
+                        </div>
+                        <div className="col col-3">
+                            <div className="group">
+                                <span className="title">From</span>
+                                <div className="input">
+                                    <DatePicker />
+                                </div>
+                            </div>
+                            <div className="group">
+                                <span className="title">Day Type</span>
+                                <div className="input">
+                                    <RadioGroup type="from-date" first="Full" second="First Half" last="Second Half" />
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col col-4">
+                            <div className="group">
+                                <span className="title">To</span>
+                                <div className="input">
+                                    <DatePicker />
+                                </div>
+                            </div>
+                            <div className="group">
+                                <span className="title">Day Type</span>
+                                <div className="input">
+                                    <RadioGroup type="to-date" first="Full" second="First Half" last="Second Half" />
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col col-5">
+                            <div className="group">
+                                <span className='title'>Reason for Leave</span>
+                                <Textarea
+                                    color="primary"
+                                    disabled={false}
+                                    minRows={4}
                                     sx={{
-                                        color: '#000',
-                                        fontFamily: 'Poppins-ExtraBold',
-                                        fontWeight: '300',
-                                        height: '50'
+                                        width: '600px',
+                                        '--Textarea-focusedHighlight': '#222bcc !important',
+                                        '&::before': {
+                                            transition: 'box-shadow .15s ease-in-out',
+                                        },
+                                        color: '#0c0048',
+                                        fontFamily: 'Poppins-Medium',
+                                        fontSize: '15px',
                                     }}
-                                    className='input'
-                                >
-                                    <MenuItem value={1}>Sri Rajasekaran</MenuItem>
-                                    <MenuItem value={2}>Kiran Wali</MenuItem>
-                                </Select>
-                            </FormControl>
-                        </div>
-                        <div className="group">
-                            <span className="title">Leaves Left</span>
-                            <span className='input'>You have used <strong>8.5 Leaves</strong> upto this point</span>
-                        </div>
-                    </div>
-                    <div className="col col-2 group">
-                        <span className="title">Leave Type</span>
-                        <div className="input">
-                            <RadioGroup type="leave-type" first="Sick Leave" second="Paid Leave" />
-                        </div>
-                    </div>
-                    <div className="col col-3">
-                        <div className="group">
-                            <span className="title">From</span>
-                            <div className="input">
-                                <DatePicker />
+                                    maxRows={4}
+                                    placeholder=""
+                                    size="lg"
+                                    variant="outlined"
+                                />
                             </div>
                         </div>
-                        <div className="group">
-                            <span className="title">Day Type</span>
-                            <div className="input">
-                                <RadioGroup type="from-date" first="Full" second="First Half" last="Second Half" />
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col col-4">
-                        <div className="group">
-                            <span className="title">To</span>
-                            <div className="input">
-                                <DatePicker />
-                            </div>
-                        </div>
-                        <div className="group">
-                            <span className="title">Day Type</span>
-                            <div className="input">
-                                <RadioGroup type="to-date" first="Full" second="First Half" last="Second Half" />
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col col-5">
-                        <div className="group">
-                            <span className='title'>Reason for Leave</span>
-                            <Textarea
-                                color="primary"
-                                disabled={false}
-                                minRows={4}
-                                sx={{
-                                    width: '600px',
-                                    '--Textarea-focusedHighlight': '#222bcc !important',
-                                    '&::before': {
-                                        transition: 'box-shadow .15s ease-in-out',
-                                    },
-                                    color: '#0c0048',
-                                    fontFamily: 'Poppins-Medium',
-                                    fontSize: '15px',
-                                }}
-                                maxRows={4}
-                                placeholder=""
-                                size="lg"
-                                variant="outlined"
-                            />
-                        </div>
-                    </div>
-                    <div className="col col-6">
-                        <Button variant="soft" sx={{
-                            width: '150px',
-                            aspectRatio: '3/0.9',
-                            backgroundColor: '#dedefe',
-                            color: '#222bcc',
-                            '&:hover': {
-                                backgroundColor: '#b8b8e5',
-                            }
-                        }}
-                            onClick={onClose}
-                        >
-                            Cancel
-                        </Button>
-                        <GitHubTooltip>
-                            <Button variant="solid" sx={{
+                        <div className="col col-6">
+                            <Button variant="soft" sx={{
                                 width: '150px',
                                 aspectRatio: '3/0.9',
-                                backgroundColor: '#222bcc',
-                                color: '#fbfcfe',
+                                backgroundColor: '#dedefe',
+                                color: '#222bcc',
                                 '&:hover': {
-                                    backgroundColor: '#595fd2;'
+                                    backgroundColor: '#b8b8e5',
                                 }
                             }}
+                                onClick={onClose}
                             >
-                                Apply
+                                Cancel
                             </Button>
-                        </GitHubTooltip>
+                            <GitHubTooltip>
+                                <Button variant="solid" sx={{
+                                    width: '150px',
+                                    aspectRatio: '3/0.9',
+                                    backgroundColor: '#222bcc',
+                                    color: '#fbfcfe',
+                                    '&:hover': {
+                                        backgroundColor: '#595fd2;'
+                                    }
+                                }}
+                                >
+                                    Apply
+                                </Button>
+                            </GitHubTooltip>
 
+                        </div>
                     </div>
-                </div>
-            </Box>
+                </Box>
+            </Grow>
         </Modal>
     );
 };
