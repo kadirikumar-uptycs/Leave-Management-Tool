a23446c94fcb461a9d698eff83317a87a926c1ac
kadirikumar-uptycs <kadirikumar@uptycs.com>
2024-05-21 00:12:40 +0530

    Updated Routes

--- Back-End-App/src/routes.js
+++ Back-End-App/src/routes.js
@@ -1,9 +1,12 @@
 let express = require('express');
+let controllers = require('./controllers');
 let router = express.Router();
 
-// Routes
-router.get('/', (req, res) => {
-    return res.status(200).send("Hello World!!!");
-})
+router.post('/user', controllers.createUser)
+router.get('/users', controllers.getUsers)
+router.put('/users/:id', controllers.editUser)
+router.delete('/user/:id', controllers.deleteUser)
+router.get('/leaveApplications', controllers.getLeaveApplications)
+router.get('/holidays', controllers.getHolidaysList)
 
 module.exports = router;
