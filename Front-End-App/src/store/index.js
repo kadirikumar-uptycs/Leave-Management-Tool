06c78d50f47868627136b7e980da8a5861d92835
kadirikumar-uptycs <kadirikumar@uptycs.com>
2024-05-31 18:46:18 +0530

    Redux Store and its Slices

---
+++ Front-End-App/src/store/index.js
@@ -0,0 +1,13 @@
+import { configureStore } from "@reduxjs/toolkit";
+import userReducer from "./userSlice";
+import authSlice from "./authSlice";
+import leaveSlice from "./leaveSlice";
+const store = configureStore({
+    reducer: {
+        user: userReducer,
+        auth: authSlice,
+        leave: leaveSlice,
+    }
+})
+
+export default store;
---
+++ Front-End-App/src/store/leaveSlice.js
@@ -0,0 +1,42 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+import config from '../config';
+
+const initialState = {
+    loading: false,
+    leaves: [],
+    error: '',
+    noData: false,
+};
+
+const fetchLeaves = createAsyncThunk('leave/fetchMyLeaves', async () => {
+    const response = await axios.get(`${config.SERVER_BASE_ADDRESS}/leaveApplications`, { withCredentials: true });
+    return response.data;
+});
+
+const leaveSlice = createSlice({
+    name: 'leaves',
+    initialState: initialState,
+    extraReducers: builder => {
+        builder.addCase(fetchLeaves.pending, state => {
+            state.loading = true;
+        });
+        builder.addCase(fetchLeaves.fulfilled, (state, action) => {
+            state.loading = false;
+            state.leaves = action.payload;
+            if(Array.isArray(action.payload) && !action.payload.length){
+                state.noData = true;
+            }else{
+                state.noData = false;
+            }
+        });
+        builder.addCase(fetchLeaves.rejected, (state, action) => {
+            state.loading = false;
+            state.leaves = [];
+            state.error = action.error.message;
+        });
+    },
+});
+
+export default leaveSlice.reducer;
+export { fetchLeaves };
