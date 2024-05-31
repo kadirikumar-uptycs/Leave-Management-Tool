833600e3f8ec807d9e0d0607f7536ef7ba8c4e40
kadirikumar-uptycs <kadirikumar@uptycs.com>
2024-05-31 18:35:06 +0530

    Added New Routes

--- Front-End-App/src/App.jsx
+++ Front-End-App/src/App.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import {
-  createBrowserRouter,
-  createRoutesFromElements,
-  Route,
-  RouterProvider
+	createBrowserRouter,
+	createRoutesFromElements,
+	Route,
+	RouterProvider
 } from "react-router-dom";
+import { Provider } from 'react-redux'
 import ErrorBoundary from './common/ErrorBoundary';
-import { SnackbarProvider } from './common/SnackBarProvider';
+import { SnackbarProvider } from './hooks/SnackBarProvider';
 import PageNotFound from './common/PageNotFound';
 import LoginPage from './Login/LoginPage';
 import HomePage from './Home/HomePage';
@@ -16,56 +17,61 @@ import Users from './Users/Users';
 import Admin from './Admin/Admin';
 import Analytics from './Analytics/Analytics';
 import Layout from './Layout';
+import store from './store';
 
 const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="/" element={<Layout />}>
-      <Route path="login" element={
-        <ErrorBoundary>
-          <LoginPage />
-        </ErrorBoundary>
-      } />
-      <Route index element={<HomePage />} />
-      <Route path="calendar" element={
-        <ErrorBoundary>
-          <Calendar />
-        </ErrorBoundary>
-      } />
-      <Route path="/leaveHistory" element={
-        <ErrorBoundary>
-          <LeavesFullHistory />
-        </ErrorBoundary>
-      } />
-      <Route path="admin" element={
-        <ErrorBoundary>
-          <Admin />
-        </ErrorBoundary>
-      } />
-      <Route path="users" element={
-        <ErrorBoundary>
-          <Users />
-        </ErrorBoundary>
-      } />
-      <Route path="analytics" element={
-        <ErrorBoundary>
-          <Analytics />
-        </ErrorBoundary>
-      } />
-      <Route path='*' element={
-        <ErrorBoundary>
-          <PageNotFound />
-        </ErrorBoundary>
-      } />
-    </Route>
-  )
+	createRoutesFromElements(
+		<Route>
+			<Route path="/" element={<Layout />}>
+				<Route index element={<HomePage />} />
+				<Route path="calendar" element={
+					<ErrorBoundary>
+						<Calendar />
+					</ErrorBoundary>
+				} />
+				<Route path="/leaveHistory" element={
+					<ErrorBoundary>
+						<LeavesFullHistory />
+					</ErrorBoundary>
+				} />
+				<Route path="admin" element={
+					<ErrorBoundary>
+						<Admin />
+					</ErrorBoundary>
+				} />
+				<Route path="users" element={
+					<ErrorBoundary>
+						<Users />
+					</ErrorBoundary>
+				} />
+				<Route path="analytics" element={
+					<ErrorBoundary>
+						<Analytics />
+					</ErrorBoundary>
+				} />
+				<Route path='*' element={
+					<ErrorBoundary>
+						<PageNotFound />
+					</ErrorBoundary>
+				} />
+			</Route>
+			<Route path="login" element={
+				<ErrorBoundary>
+					<LoginPage />
+				</ErrorBoundary>
+			} />
+		</Route>
+	)
 );
 
 export default function App() {
-  return (
-    <SnackbarProvider>
-      <ErrorBoundary>
-        <RouterProvider router={router} />
-      </ErrorBoundary>
-    </SnackbarProvider>
-  );
+	return (
+		<Provider store={store}>
+			<SnackbarProvider>
+				<ErrorBoundary>
+					<RouterProvider router={router} />
+				</ErrorBoundary>
+			</SnackbarProvider>
+		</Provider>
+	);
 }
