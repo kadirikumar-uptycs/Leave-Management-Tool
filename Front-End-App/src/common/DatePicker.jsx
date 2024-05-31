b6a145ce315244fcc2cb8c1a388a0bf6b8123860
kadirikumar-uptycs <kadirikumar@uptycs.com>
2024-05-31 18:41:42 +0530

    Date Picker component

--- Front-End-App/src/common/DatePicker.jsx
+++ Front-End-App/src/common/DatePicker.jsx
@@ -3,15 +3,19 @@ import { DemoContainer } from '@mui/x-date-pickers/internals/demo';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { DatePicker as CustomDatePicker } from '@mui/x-date-pickers/DatePicker';
-
-const DatePicker = () => {
+import { formatDateToStandard } from '../common/getCurrentDate';
+const DatePicker = ({ onDateChange }) => {
     return (
         <LocalizationProvider dateAdapter={AdapterDayjs}>
             <DemoContainer components={['DatePicker']} sx={{
                 fontFamily: 'Poppins-ExtraBold'
             }}>
                 <CustomDatePicker
+                    disablePast
                     slotProps={{ textField: { size: 'small', fullWidth: false } }}
+                    onChange={(value) => {
+                        onDateChange(formatDateToStandard(value));
+                    }}
                 />
             </DemoContainer>
         </LocalizationProvider>
