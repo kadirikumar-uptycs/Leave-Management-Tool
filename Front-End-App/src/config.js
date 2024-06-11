d71da4baee01c46d2fcecc4f173d98e6556784ca
kadirikumar-uptycs <kadirikumar@uptycs.com>
2024-06-11 17:06:39 +0530

    Updated Server URL

--- Front-End-App/src/config.js
+++ Front-End-App/src/config.js
@@ -1,7 +1,5 @@
 let config = {};
 
-if (process.env.NODE_ENV === 'production')
-    config.SERVER_BASE_ADDRESS = "https://server-cxtools.uptycs.dev";
-else
-    config.SERVER_BASE_ADDRESS = "http://localhost:17291";
+config.SERVER_BASE_ADDRESS = "http://localhost:17291";
+
 export default config;
